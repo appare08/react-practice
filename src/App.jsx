9db199fac260740ab69068a8213dcbe646c4c3f2
appare08 <87957526+appare08@users.noreply.github.com>
2021-08-02 12:20:57 +0900

    props usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+
 const App = () => {
-  const onClickButton = () => {
-    alert("こんにちは");
+  const onClickCountup = () => {
+    setNum(num + 1);
   };
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
+
+  const [num, setNum] = useState(0);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue" message="お元気ですか？" />
+      <ColorfulMessage color="pink" message="元気です" />
+      <button onClick={onClickCountup}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,11 @@
+const ColorfulMessage = (props) => {
+  const { color, message } = props;
+  const contentStyle = {
+    color: color,
+    fontSize: "18px"
+  };
+
+  return <p style={contentStyle}>{message}</p>;
+};
+
+export default ColorfulMessage;
